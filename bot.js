9da87f216e72a07546db60f08656dda2586c6365
Mehdi7894 <47418152+Mehdi7894@users.noreply.github.com>
2019-02-08 16:27:32 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,51 +1,22 @@
-client.on("message",async message => {
-if(message.content === 'shop'){//الامر
-let staff = message.guild.member(message.author).roles.find('name' ,"اسم رتبت البيع");
-      if(!staff) return message.reply(**Only Sellers | :x:**)
-var shopc = message.guild.channels.find("name","اسم الروم")
-  if(!shopc) return message.reply("لا اجد الروم المخصص للبيع")
-    let shop = '';
-      let fillter = m => m.author.id === message.author.id
+client.on('guildMemberAdd', member => {
 
+    const channel = member.guild.channels.find('name', '・text');
 
-
-      await message.channel.send("اكتب الاشياء الذي سوف تبيعها").then(e => {
-           message.channel.awaitMessages(fillter, { time: 60000, max: 1
-})
-     .then(co => {
-       shop = co.first().content;
-        co.first().delete();
-
-let desc = '';
-
-e.edit("اكتب الدفع عند مين؟").then(e => {
-  message.channel.awaitMessages(fillter, { time: 60000, max: 1 })
-
-     .then(co => {
-       desc = co.first().content;
-        co.first().delete();
-e.edit("Done").then(e => {
-  shopc.send(@everyone <$> @here
-${message.guild.name}:tm: Shop :arrow_down:
-======================
-${shop}
-=================
-**الدفع عند:** **${desc}**
-
-**تم الارسال بواسطة:** ${message.author}
-@everyone </> @here)
-  })
-})
-  })
-})
-  })
+    const millis = new Date().getTime() - member.user.createdAt.getTime();
+    const now = new Date();
+    const createdAt = millis / 1000 / 60 / 60 / 24;
 
 
 
 
 
+    const embed = new Discord.RichEmbed()
+
+    .setColor("black")
+    .setDescription(**تاريخ دخولك للدسكورد منذ ${createdAt.toFixed(0)} يوم**)
+    .setAuthor(member.user.tag, member.user.avatarURL);
+    channel.sendEmbed(embed);
 
 
-}
 });
-client.login(token here);
+client.login("token here");
